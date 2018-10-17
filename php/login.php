b4e0358c8d06c3a1c85b28fdc35d5f2ed62104aa
yangchengyi11 <360886965@qq.com>
2018-10-17 14:41:04 +0800

    小米商城

--- php/login.php
+++ php/login.php
@@ -1,28 +1,22 @@
 <?php
     // 获取用户名
     header("Content-Type: application/json");
-
     header("Access-Control-Allow-Origin:*");
-
-    // 引用另外一个文件
     include "public/connect_db.php";
-    
-    // 或去json数据, 请求主体的格式为json格式
     $json = json_decode(file_get_contents("php://input"));
     $username = $json -> username;
     $password = $json -> password;
-    // 链接数据库
     $coon = new db();
-    $sql = "select * from `shop_user` WHERE name='$username' and  password='$password'";
-    $rows = $coon -> Query($sql, 2);
-    if($rows) {
-      // 用户输入正确
-      $arr = array("code" => "200", "msg"=>"", "data"=>array("id"=>$rows["id"], "token"=>"1234567899", "atavar"=> "http://www.aaa.com/path/a.png"));
+    $sql="SELECT * from shop WHERE username = '$username' and password = '$password'";
+    $row = $coon->Query($sql, 2);
+    // 找到数据
+    if($row) {
+      $arr = array("id" => $row["id"], "username"=> $row["username"]);
+      // 返回用户基本信息
+      $array = array("code"=>"200", "msg"=> "", "data"=>  $arr);
+      
     } else {
-      // 输入错误
-      $arr = array("code" => "1000", "msg" => "用户名或密码输入错误");
+      $array = array("code"=>"1001", "msg"=> "账号或者密码错误！！");
     }
-    echo json_encode($arr);
-
-   
+    echo json_encode($array);
   ?>
